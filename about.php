3f27c1547f392339f14cd6808d69549c2a1813ba
lsh2939 <adgjl2939@naver.com>
2021-01-22 05:41:23 +0900

    css폴더 js폴더 만들기

--- about.php
+++ about.php
@@ -1,16 +1,17 @@
 <?php
 require_once "head.php";
 ?>
+<link rel="stylesheet" href="CSS/about.css">
+<script src="JS/about.js" defer></script>
 
-
-      <section class="section-title con-min-width">
-        <h1 class="con">
-          <span><i class="far fa-hand-point-up"></i></span>
-          <span>
-            ABOUT</span>
-        </h1>
-      </section>
+<section class="section-title con-min-width">
+  <h1 class="con">
+    <span><i class="far fa-hand-point-up"></i></span>
+    <span>
+      ABOUT</span>
+  </h1>
+</section>
 
 <?php
 require_once "foot.php";
-?>   
+?>
